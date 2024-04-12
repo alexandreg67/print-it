c7f0231b2feb2f3323b9c9b5ee32d4f6ead81aef
Graff Alexandre <alexgraff67@gmail.com>
2024-04-12 14:41:22 +0200

    0.2 mise en place de la navigation des images + affichage dot

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,9 @@
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
+const dots = document.querySelector('.dots');
+const bannerImg = document.querySelector('.banner-img');
+let i = 0;
 
-
-arrowLeft.addEventListener('click', () => {
-	console.log('click left');
-});
-
-arrowRight.addEventListener('click', () => {
-	console.log('click right');
-});
 
 const slides = [
 	{
@@ -29,4 +24,26 @@ const slides = [
 	}
 ]
 
+
+dots.innerHTML = '<span class="dot"></span>'.repeat(slides.length);
+
+arrowLeft.addEventListener('click', () => {
+	console.log('click left');
+	i--;
+	if(i < 0){
+		i = slides.length - 1;
+	}
+	bannerImg.src = `./assets/images/slideshow/${slides[i].image}`;
+});
+
+arrowRight.addEventListener('click', () => {
+	console.log('click right');
+	i++;
+	if(i >= slides.length){
+		i = 0;
+	}
+	src="./assets/images/slideshow/slide1.jpg";
+	bannerImg.src = `./assets/images/slideshow/${slides[i].image}`;
+
+});
 
