6ea2e7580fa7164499ab892cbeab681aa55f1508
Graff Alexandre <alexgraff67@gmail.com>
2024-04-13 10:37:35 +0200

    0.4 mise en place de la navigation des points

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const arrowRight = document.querySelector('.arrow_right');
 const dots = document.querySelector('.dots');
 const bannerImg = document.querySelector('.banner-img');
 const banner = document.querySelector('#banner');
-let i = 0;
+let index = 0;
 
 
 const slides = [
@@ -26,38 +26,39 @@ const slides = [
 ]
 
 
-dots.innerHTML = '<span class="dot"></span>'.repeat(slides.length);
+dots.innerHTML = `<span class="dot"></span>`.repeat(slides.length);
+dots.children[index].classList.add('dot_selected');
+
 
 arrowLeft.addEventListener('click', () => {
-	console.log('click left');
 	let tagLine = document.querySelector('.tag-line');
-	i--;
-	if(i < 0){
-		i = slides.length - 1;
+	dots.children[index].classList.remove('dot_selected');
+	index--;
+	if(index < 0){
+		index = slides.length - 1;
 	}
-	bannerImg.src = `./assets/images/slideshow/${slides[i].image}`;
+	bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
 	let texte = document.createElement('p');
-	texte.innerHTML = `${slides[i].tagLine}`;
+	texte.innerHTML = `${slides[index].tagLine}`;
 	texte.classList.add('tag-line');
 	banner.removeChild(tagLine);
 	banner.appendChild(texte);
-	console.log(texte);
-	
+	dots.children[index].classList.add('dot_selected');
 });
 
 arrowRight.addEventListener('click', () => {
-	// console.log('click right');
 	let tagLine = document.querySelector('.tag-line');
-	i++;
-	if(i >= slides.length){
-		i = 0;
+	dots.children[index].classList.remove('dot_selected');
+	index++;
+	if(index >= slides.length){
+		index = 0;
 	}
-	bannerImg.src = `./assets/images/slideshow/${slides[i].image}`;
+	bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
 	let texte = document.createElement('p');
-	texte.innerHTML = `${slides[i].tagLine}`;
+	texte.innerHTML = `${slides[index].tagLine}`;
 	texte.classList.add('tag-line');
-	// console.log(texte);
 	banner.removeChild(tagLine);
 	banner.appendChild(texte);
+	dots.children[index].classList.add('dot_selected');
 });
 
